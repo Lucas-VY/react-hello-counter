da53da10c1f25954b6d3f2a8114e8f3f80b989df
LucasVY <lucasvy@LucasVYs-MacBook-Pro.local>
2021-03-31 17:02:29 -0300

    react counter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Counter from  './components/SecondsCounter'
+import './index.css';
+
+function App() {
+  return (
+    <>
+    <Counter />
+    </>
+      
+  );
+}
+
+export default App;
---
+++ src/components/SecondsCounter.js
@@ -0,0 +1,28 @@
+import React from "react";
+import propTypes from 'prop-types';
+
+const secondsCounter = props => {
+    return (
+    <div className="bigCounter">
+        <div className="calendar">
+            <i className="far fa-clock"></i>
+        </div>
+        <div className="six">{props.digitSix %10}</div>
+        <div className="five">{props.digitFive %10}</div>
+        <div className="four">{props.digitFour %10}</div>
+        <div className="three">{props.digitThree %10}</div>
+        <div className="two">{props.digitTwo %10}</div>
+        <div className="one">{props.digitOne %10}</div>
+    </div>
+    );
+}
+
+secondsCounter.propTypes={
+    digitOne: propTypes.number,
+    digitTwo: propTypes.number,
+    digitThree: propTypes.number,
+    digitFour: propTypes.number,
+    digitFive: propTypes.number,
+    digitSix: propTypes.number,
+}
+export default secondsCounter;
