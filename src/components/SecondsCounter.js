d9e32b66e93201e3d01224c5d877fc42f5e40359
LucasVY <lucasvy@LucasVYs-MacBook-Pro.local>
2021-03-31 18:25:01 -0300

    clock counter

--- src/components/SecondsCounter.js
+++ src/components/SecondsCounter.js
@@ -3,17 +3,17 @@ import propTypes from 'prop-types';
 
 const secondsCounter = props => {
     return (
-    <div className="bigCounter">
-        <div className="calendar">
-            <i className="far fa-clock"></i>
+        <div className="bigCounter">
+            <div className="calendar">
+                <i className="far fa-clock"></i>
+            </div>
+            <div className="six">{props.digitSix %10}</div>
+            <div className="five">{props.digitFive %10}</div>
+            <div className="four">{props.digitFour %10}</div>
+            <div className="three">{props.digitThree %10}</div>
+            <div className="two">{props.digitTwo %10}</div>
+            <div className="one">{props.digitOne %10}</div>    
         </div>
-        <div className="six">{props.digitSix %10}</div>
-        <div className="five">{props.digitFive %10}</div>
-        <div className="four">{props.digitFour %10}</div>
-        <div className="three">{props.digitThree %10}</div>
-        <div className="two">{props.digitTwo %10}</div>
-        <div className="one">{props.digitOne %10}</div>
-    </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -23,14 +23,10 @@ setInterval(function(){
   counter++;
 
   ReactDOM.render(
-    <React.StrictMode>
-      <SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six}/>
-    </React.StrictMode>,
+      <SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six}/>,
     document.getElementById('root')
   );
 },1000);;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
